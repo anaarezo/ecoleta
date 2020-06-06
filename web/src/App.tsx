7050cb3f6f777d131b2a9b1a3aae4c4e6b6de02a
anaarezo <laura.arezo@gmail.com>
2020-06-06 18:32:16 -0300

    NLW-003 Feat: useState

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './Header';
 
 // estados são informações mantidas pelo próprio componente - clicou ou preencheu algum input
 // JSX Sintaxe de xml dentro do javascript - props atributos que eu quero enviar para o componente
 function App() {
-  
-  let counter = 1;
+  // Retorna sempre array [valor do estado, função para atualizar o valor do estado]
+  const [counter, setCounter] = useState(0); 
 
   function handleButtonClick() {
-    counter++;
-    console.log(counter);
+    setCounter(counter + 1);
   }
 
   return (
@@ -18,6 +17,7 @@ function App() {
       <Header title="Hello World"/>
       
       <h1>{counter}</h1>
+      <h1>{counter * 2}</h1>
       <button type="button" onClick={handleButtonClick}>Aumentar</button>
     </div>
   );
