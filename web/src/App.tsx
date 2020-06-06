f957325e778661f985ba4c0391d7e23fb240c502
anaarezo <laura.arezo@gmail.com>
2020-06-06 18:34:50 -0300

    NLW-003 Feat: useState

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import Header from './Header';
 
-// estados são informações mantidas pelo próprio componente - clicou ou preencheu algum input
+// imutabilidade/estados são informações mantidas pelo próprio componente - clicou ou preencheu algum input
 // JSX Sintaxe de xml dentro do javascript - props atributos que eu quero enviar para o componente
 function App() {
   // Retorna sempre array [valor do estado, função para atualizar o valor do estado]
@@ -14,10 +14,9 @@ function App() {
 
   return (
     <div>
-      <Header title="Hello World"/>
+      <Header title={`Contador: ${counter}`}/>
       
       <h1>{counter}</h1>
-      <h1>{counter * 2}</h1>
       <button type="button" onClick={handleButtonClick}>Aumentar</button>
     </div>
   );
